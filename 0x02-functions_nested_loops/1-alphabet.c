1a1a321fd7d62a99d2b7ca66180ebe37127a4269
miyeakinyele <miyeakinyele@gmail.com>
2023-02-21 15:02:25 -0800

    print alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,12 @@
 #include "main.h"
 
 /**
- * main - print_alphabet
  *
  * print_alphabet - prints the alphabet in lowercase, followed by anew line.
  *
  * return: alawys 0;
  */
-void print_alphabet(void);
+void print_alphabet(void)
 {
 	char letter;
 
