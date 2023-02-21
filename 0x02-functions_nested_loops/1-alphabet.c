193e0b131961d0de171648fb6bec0b89dbf2bfd6
miyeakinyele <miyeakinyele@gmail.com>
2023-02-21 15:00:23 -0800

    print alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,19 +2,18 @@
 
 /**
  * main - print_alphabet
- * print_alphabet fuction that prints alphanet in lowercase
+ *
+ * print_alphabet - prints the alphabet in lowercase, followed by anew line.
  *
  * return: alawys 0;
  */
-
-
-void print_alphabet(void)
-
+void print_alphabet(void);
 {
 	char letter;
 
 	for (letter = 'a'; letter <= 'z'; letter++)
 		_putchar(letter);
+
 	_putchar('\n');
 }
 
