b65ac187c384dc57ab5f46e86b687c927e5682d8
miyeakinyele <miyeakinyele@gmail.com>
2023-02-23 13:06:05 -0800

    diagonal

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,9 +1,10 @@
 #include "main.h"
 
 /**
-*print_most_numbers - prints
-* owned by Bwave ICT
-*Return: void
+ * print_most_numbers - prints
+ *
+ * Return: void
+ *
 */
 
 void print_most_numbers(void)
